5d2a31f57aa392c2181c7f67bd4c1165f1050fcb
Komalgupta8 <komalguptagupta234@gmail.com>
2024-12-29 16:25:51 +0530

    initial commit

---
+++ DataType.js
@@ -0,0 +1,14 @@
+"use Strict" // treat all js code as newer version
+
+// number => 2 to power 53
+// bigint =>
+// string =>""
+// boolean
+// null => stand alone value , it is a object also
+//symbol =>unique
+//undefined =>
+
+//object
+
+console.log(typeof null) // object
+
