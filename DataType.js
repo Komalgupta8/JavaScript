4a38d865f4373c1b4d6b62233139e34dcfd924c1
Komalgupta8 <komalguptagupta234@gmail.com>
2025-01-11 22:52:27 +0530

    datatype

--- DataType.js
+++ DataType.js
@@ -12,3 +12,34 @@
 
 console.log(typeof null) // object
 
+
+// primitive datatypes
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+//reference (Non - Primitive) --> Arrays , object , funtion
+
+const heros=["cat" , "dog"]
+
+let obj={
+    1 : "komal",
+    2 :"kajal"
+}
+
+const myfn=function(){
+    console.log("fun")
+}
+
+
