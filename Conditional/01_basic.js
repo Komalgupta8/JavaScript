e2e2b2aea53b705fbbfc67ef7fbdd63897822916
komalgupta8 <komalguptagupta234@gmail.com>
2025-01-26 14:46:10 +0530

    conditional

---
+++ Conditional/01_basic.js
@@ -0,0 +1,49 @@
+// switch(key){
+//     case value:
+
+//         break;
+
+//     default :
+//         break;
+// }
+
+const month = 3;
+
+switch (month) {
+    case 1:
+        console.log("jan")
+        break;
+    case 2:
+        console.log("feb")
+        break;
+    case 3:
+        console.log("march")
+        break;
+    default:
+        console.log("dafault")
+        break;
+}
+
+
+// falsy Values
+
+// false ,0 , -0 ,BigInt 0n, null , Undefined , NaN , ""
+
+// truthy values
+
+// "0" , 'false' ," " , [] , {} , function(){} 
+
+// Nullish coalescing oprt(??)
+
+let val1 =3
+val1 = 5 ?? 10
+val1 =  null?? 10
+
+
+
+
+
+
+
+
+
